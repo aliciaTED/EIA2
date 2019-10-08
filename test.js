c3526011fbb01c2ab7a3ea528bb849b41f1661fa
aliciaTED <48830896+aliciaTED@users.noreply.github.com>
2019-10-08 10:03:05 +0200

    testtest

--- test.js
+++ test.js
@@ -2,4 +2,9 @@
 let v = 1;
 v = v + 1;
 console.log(v);
+let v1 = 4;
+let v2 = 5;
+v2 = v1;
+v1 = 3;
+console.log(v2);
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,3 +1,9 @@
-let v = 1;
+let v: number = 1;
 v = v + 1;
 console.log(v);
+
+let v1 = 4;
+let v2 = 5;
+v2 = v1;
+v1 = 3;
+console.log(v2);
