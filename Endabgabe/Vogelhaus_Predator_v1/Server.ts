18761a775c2098ec23ea517638d63b63bea6e5f4
aliciaTED <48830896+aliciaTED@users.noreply.github.com>
2020-02-10 16:31:18 +0100

    server datei updated

--- Endabgabe/Vogelhaus_Predator_v1/Server.ts
+++ Endabgabe/Vogelhaus_Predator_v1/Server.ts
@@ -3,13 +3,18 @@ import * as Url from "url";
 import * as Mongo from "mongodb";
 
 export namespace Endabgabe {
+
     let highscoreList: Mongo.Collection;
+    // let databaseURL: string;
+
+    // let dbName: string = "birdhouse";
+    // let dbCollection: string = "highscoreList";
+
+    let databaseUrl: string = "mongodb+srv://zero-x:Inverted456@cluster0-ldpu0.mongodb.net/test?retryWrites=true&w=majority";
 
     let port: number | string | undefined = process.env.PORT;
     if (port == undefined)
         port = 5001;
-
-    let databaseUrl: string = "mongodb+srv://zero-x:Inverted456@cluster0-ldpu0.mongodb.net/test?retryWrites=true&w=majority";
 
     startServer(port);
     connectToDatabase(databaseUrl);
@@ -23,14 +28,14 @@ export namespace Endabgabe {
     }
 
     async function connectToDatabase(_url: string): Promise<void> {
-        let options: Mongo.MongoClientOptions = {useNewUrlParser: true, useUnifiedTopology: true};
+        let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
         let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
         await mongoClient.connect();
         highscoreList = mongoClient.db("birdhouse").collection("highscoreList");
         console.log("Database connection ", highscoreList != undefined);
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise<void> {
         console.log("What's up?");
 
         _response.setHeader("content-type", "text/html; charset=utf-8");
@@ -38,17 +43,36 @@ export namespace Endabgabe {
 
         if (_request.url) {
             let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-            for (let key in url.query) {
-                _response.write(key + ":" + url.query[key] + "<br/>");
+            if (url.query["command"] == "retrieve") {
+                let report: any[] | string = await retrieveHighscore();
+                if (report == "We encountered technical problems. Please try again later.")
+                    _response.write(report);
+                else
+                    _response.write(JSON.stringify(report));
             }
-
-            let jsonString: string = JSON.stringify(url.query);
-            _response.write(jsonString);
-
-            highscoreList.insert(url.query);
+            else {
+                console.log("URL-Query: ", url.query);
+                let jsonString: string = JSON.stringify(url.query);
+                _response.write(jsonString);
+                highscoreList.insert(url.query);
+                console.log(jsonString);
+            }
         }
-
-        _response.write("this is my response");
         _response.end();
     }
+
+    async function retrieveHighscore(): Promise<any[] | string> {
+        let cursor: Mongo.Cursor = await highscoreList.find();
+        let answer: Promise<any[]> = await cursor.toArray();
+        console.log("DB CursorToArray", answer);
+        if (answer != null) {
+            return answer;
+        }
+        else {
+            return "We encountered technical problems. Please try again later.";
+        }
+    }
+
 }
+
+
--- Endabgabe/Vogelhaus_Predator_v1/server.js
+++ Endabgabe/Vogelhaus_Predator_v1/server.js
@@ -6,10 +6,13 @@ const Mongo = require("mongodb");
 var Endabgabe;
 (function (Endabgabe) {
     let highscoreList;
+    // let databaseURL: string;
+    // let dbName: string = "birdhouse";
+    // let dbCollection: string = "highscoreList";
+    let databaseUrl = "mongodb+srv://zero-x:Inverted456@cluster0-ldpu0.mongodb.net/test?retryWrites=true&w=majority";
     let port = process.env.PORT;
     if (port == undefined)
         port = 5001;
-    let databaseUrl = "mongodb+srv://zero-x:Inverted456@cluster0-ldpu0.mongodb.net/test?retryWrites=true&w=majority";
     startServer(port);
     connectToDatabase(databaseUrl);
     function startServer(_port) {
@@ -25,21 +28,39 @@ var Endabgabe;
         highscoreList = mongoClient.db("birdhouse").collection("highscoreList");
         console.log("Database connection ", highscoreList != undefined);
     }
-    function handleRequest(_request, _response) {
+    async function handleRequest(_request, _response) {
         console.log("What's up?");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
             let url = Url.parse(_request.url, true);
-            for (let key in url.query) {
-                _response.write(key + ":" + url.query[key] + "<br/>");
+            if (url.query["command"] == "retrieve") {
+                let report = await retrieveHighscore();
+                if (report == "We encountered technical problems. Please try again later.")
+                    _response.write(report);
+                else
+                    _response.write(JSON.stringify(report));
             }
-            let jsonString = JSON.stringify(url.query);
-            _response.write(jsonString);
-            highscoreList.insert(url.query);
+            else {
+                console.log("URL-Query: ", url.query);
+                let jsonString = JSON.stringify(url.query);
+                _response.write(jsonString);
+                highscoreList.insert(url.query);
+                console.log(jsonString);
+            }
         }
-        _response.write("this is my response");
         _response.end();
+    }
+    async function retrieveHighscore() {
+        let cursor = await highscoreList.find();
+        let answer = await cursor.toArray();
+        console.log("DB CursorToArray", answer);
+        if (answer != null) {
+            return answer;
+        }
+        else {
+            return "We encountered technical problems. Please try again later.";
+        }
     }
 })(Endabgabe = exports.Endabgabe || (exports.Endabgabe = {}));
 //# sourceMappingURL=Server.js.map
